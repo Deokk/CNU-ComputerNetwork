a7a6319f566a098f39b05dd794aba89540711ca5
Deokk <deokk1112@gmail.com>
2021-01-11 04:15:00 +0900

    Add CN-09

---
+++ ComputerNetwork/week_9/config/passport.js
@@ -0,0 +1,25 @@
+var passport         = require('passport');
+var GoogleStrategy   = require('passport-google-oauth2').Strategy;
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new GoogleStrategy(
+  {
+    clientID      : '',
+    clientSecret  : '',
+    callbackURL   : '/auth/google/callback',
+    passReqToCallback   : true
+  }, function(request, accessToken, refreshToken, profile, done){
+    console.log('profile: ', profile);
+    var user = profile;
+
+    done(null, user);
+  }
+));
+
+module.exports = passport;
