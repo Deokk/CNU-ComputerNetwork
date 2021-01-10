33a4b64bd8029f05b0080be225917e92354bfd76
Deokk <deokk1112@gmail.com>
2021-01-11 04:10:25 +0900

    Add CN-06

---
+++ ComputerNetwork/week_6/routes/home.js
@@ -0,0 +1,13 @@
+// Request가 발생했을 때 서버에서 어떤 작업을 할 지 결정
+module.exports = function(app) {
+
+    app.get('/', function(req, res) {
+        res.render('welcome.ejs')
+    });
+
+    app.get('/about', function(req, res) {
+        res.render('about.ejs');
+    })
+}
+
+
